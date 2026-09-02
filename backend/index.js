6c6c81a85a6c2a0350d4d67472a02e2aaf8a86ed
Nikita Petrenko <nikita.petrenko@ynov.com>
2026-09-02 17:06:55 +0200

    connect router to index

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors    = require('cors');
+const router  = require('./router/router.js');
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const router  = require('./router/router.js');
+
+app.use(cors({ origin: '*', credentials: true }));
+app.use(express.json());
+
+app.use('/api', router);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
