c72315dffc4d3b7d6424c22664ff682cddf5abaa
Quentin <quentin.guyenot@ynov.com>
2026-09-02 17:12:16 +0200

    feat: création de la fonction getArtist

--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -21,3 +21,22 @@ const getArtworks = async (req, res) => {
         return fail(res, 500, err.message)
     }
 }
+
+const getArtiste = async (req, res) => {
+    const { page, limit } = req.body;
+
+    if (!page, !limit) {
+        return fail(res, 400, 'page et limit requis')
+    }
+
+    try {
+        const response = await fetch(`https://api.artic.edu/api/v1/artworks?fields=${id},artist_display`);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+         const result = await response.json();
+        return ok(res, result)
+    } catch (err) {
+        return fail(res, 500, err.message)
+    }
+}
