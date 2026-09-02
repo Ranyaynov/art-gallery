98823b719c99057d1807d5e380ed4e486a33efa3
Harold <harold.francois@ynov.com>
2026-09-02 17:34:01 +0200

    fix: correction des appels fetchs vers le backend

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,7 +1,7 @@
 // Fichier qui demande au serveur les données des artistes / oeuvres
 async function fetchArtworks() {
     try {
-        const response = await fetch('http://localhost:3000/api/oeuvres');
+        const response = await fetch('http://localhost:3000/api/artworks');
         const data = await response.json();
         return data; // Renvoie juste les données (images, artistes)
     } catch (error) {
