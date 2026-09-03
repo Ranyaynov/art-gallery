860c12723c8bbfa300acddec44e9e9c262bf50f2
Quentin <quentin.guyenot@ynov.com>
2026-09-03 12:31:47 +0200

    fix: correction de l'orthographe en getArtist

--- backend/router/router.js
+++ backend/router/router.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router  = express.Router();
 
 const { getArtworks } = require('../controller/controller');
-const { getArtiste } = require('../controller/controller');
+const { getArtist } = require('../controller/controller');
 
 router.get("/artworks", getArtworks);
-router.get("/artiste", getArtiste);
+router.get("/artiste", getArtist);
 
 module.exports = router;
