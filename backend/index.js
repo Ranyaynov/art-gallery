40577cb792d178c12e671e8800b9ce8c0f4a287a
Harold <harold.francois@ynov.com>
2026-09-10 15:12:49 +0200

    fix: ajouts des fichiers statics servis par le serveur

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const router = require('./router/router.js');
 
 const app = express()
 const port = 3001
 
-
 app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
 app.use('/api', router);
 
